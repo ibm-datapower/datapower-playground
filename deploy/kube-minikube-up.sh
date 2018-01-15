50474dc130054b4fdfb20c67d2780059b8244095
Anthony Ffrench <tffrench@us.ibm.com>
2018-01-15 11:02:36 -0500

    update the minikube helper

--- deploy/kube-minikube-up.sh
+++ deploy/kube-minikube-up.sh
@@ -2,12 +2,9 @@
 
 cd "$(dirname "$0")"
 
-status=$(minikube status | grep ^minikubeVM | cut -d " " -f2)
+status=$(minikube status | grep ^minikube | cut -d " " -f2)
 if [ $status != "Running" ]; then
-    minikube start \
-      --cpus=4 \
-      --memory=4096 \
-      --iso-url=https://github.com/boot2docker/boot2docker/releases/download/v1.12.5/boot2docker.iso
+    minikube start --cpus 4 --memory 8192 --kubernetes-version v1.8.6 --vm-driver hyperkit --bootstrapper kubeadm
 else
     echo "minikube running"
 fi
